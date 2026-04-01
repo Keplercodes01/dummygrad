6c58b7ea6db436b197b7fcbeb4d4f4bcc3a5516b
Kepler <keplerearth6@gmail.com>
2026-04-01 12:38:03 +0530

    mlp progress

--- src/mlp.cpp
+++ src/mlp.cpp
@@ -12,6 +12,9 @@
 #include <vector>
 #include <string>
 #include <random>
+#include<map>
+#include<set>
+#include<fstream>
 
 std::vector<std::string> load_words(const std::string& filename) {
     std::vector<std::string> words;
@@ -34,6 +37,25 @@ int main() {
     std::cout << "dummygrad MLP" << std::endl;
     auto words = load_words("/content/dummygrad/src/names.txt");
     std::cout<< "loaded" << words.size() << "words" << std::endl;
+
+    //build vocab
+    std::map<char, int> stoi;
+    std::map<int, char> itos;
+
+    std::set<char> chars;
+    for(auto& w : words) for(char c : w) chars.insert(c);
+
+    int idx = 1;
+    for(char c : chars) {
+        stoi[c] = idx;
+        itos[idx] = c;
+        idx++;
+    }
+    stoi['.'] = 0;
+    itos[0] = '.';
+    int vocab_size = stoi.size();
+
+    std::cout<"vocab size: " << vocab_size <<std::endl;
     
     return 0;
 }
